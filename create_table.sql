0dda23ecc35443bdee75f3eababe8c4b7b164f81
oizy404 <71779024+oizy404@users.noreply.github.com>
2020-12-06 00:42:34 +0800

    SQL CREATE TABLE

---
+++ create_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE locations (
+	location_id INT (11) AUTO_INCREMENT PRIMARY KEY,
+	street_address VARCHAR (40) DEFAULT NULL,
+	postal_code VARCHAR (12) DEFAULT NULL,
+	city VARCHAR (30) NOT NULL,
+	state_province VARCHAR (25) DEFAULT NULL,
+	country_id CHAR (2) NOT NULL,
+	FOREIGN KEY (country_id) REFERENCES countries (country_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
