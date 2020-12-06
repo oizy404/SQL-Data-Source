a0c82d5318297b120da3abd491fe9fb335d4ca7c
oizy404 <71779024+oizy404@users.noreply.github.com>
2020-12-07 02:05:24 +0800

    SQL CONCAT

--- contcat.sql
+++ contcat.sql
@@ -1,4 +1,4 @@
-SELECT CONCAT(city,', ', state_provincestate_province) FROM locations
+SELECT CONCAT(city,', ', state_province) FROM locations
 WHERE location_id = 1500;
 
 SELECT city || ', ' || state_provincestate_province FROM locations
