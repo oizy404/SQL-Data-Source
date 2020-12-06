89422fb0238a04b941a870376abd9da322cf6fcb
oizy404 <71779024+oizy404@users.noreply.github.com>
2020-12-06 23:54:34 +0800

    SQL DELETE FROM

--- delete.sql
+++ delete.sql
@@ -1,7 +1,7 @@
-DELETE FROM employees
-WHERE employee_id = 206;
+DELETE FROM regions
+WHERE region_id = 4;
 
 DELETE FROM employees
 WHERE employee_id IN
 (SELECT employee_id FROM dependents
-WHERE dependent_id = 1);
+WHERE dependent_id = 9);
