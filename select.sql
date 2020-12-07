0d5e05a3124a114c5915a3282072cbd100ed32d4
oizy404 <71779024+oizy404@users.noreply.github.com>
2020-12-07 13:26:46 +0800

    SQL SELECT

--- select.sql
+++ select.sql
@@ -1,17 +1,3 @@
 SELECT `department_name`FROM `departments`;
 SELECT `department_id`, `department_name` FROM `departments`;
 SELECT * FROM `departments`
-
-#### Table `departments`
-
-![image](https://user-images.githubusercontent.com/71779024/101282938-c922cd00-3812-11eb-91e9-fd7109f43a05.png)
-#### Result:
-* Retrieve one column
-
-  ![image](https://user-images.githubusercontent.com/71779024/101282923-b3ada300-3812-11eb-9d79-ef813711d69d.png)
-*  Retrieve multiple columns
-
-  ![image](https://user-images.githubusercontent.com/71779024/101282906-9aa4f200-3812-11eb-8135-6f7c768abde2.png)
-* Retrieve all columns
-
-  ![image](https://user-images.githubusercontent.com/71779024/101282938-c922cd00-3812-11eb-91e9-fd7109f43a05.png)
