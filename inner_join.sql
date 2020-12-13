32b2077ff4aeed9eb7c64b2d199944c54c0d9558
oizy404 <71779024+oizy404@users.noreply.github.com>
2020-12-13 21:48:27 +0800

    SQL INNER JOIN

--- inner_join.sql
+++ inner_join.sql
@@ -1,5 +1,5 @@
 SELECT countries.country_id, locations.location_id
   FROM countries
   INNER JOIN locations
-  ON countries.country_id= locations.country_id
+  ON countries.country_id = locations.country_id
   ORDER BY countries.country_id;
