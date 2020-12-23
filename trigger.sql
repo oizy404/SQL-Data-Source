a20c5d9bb4989db03f576cdf6de1b9524f301c39
oizy404 <71779024+oizy404@users.noreply.github.com>
2020-12-23 23:53:15 +0800

    SQL TRIGGER

--- trigger.sql
+++ trigger.sql
@@ -1,7 +1,5 @@
 ALTER TABLE locations ADD COLUMN complete_address VARCHAR(100);
-
 UPDATE locations SET complete_address = CONCAT(street_address, ' - ', city, ', ', state_province);
 
 CREATE TRIGGER insert_trigger BEFORE INSERT ON locations FOR EACH ROW SET new.complete_address = CONCAT(new.street_address, ' - ', new.city, ', ', new.state_province);
-
 CREATE TRIGGER update_trigger BEFORE UPDATE ON locations FOR EACH ROW SET new.complete_address = CONCAT(new.street_address, ' - ', new.city, ', ', new.state_province);
