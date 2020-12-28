36517c1311fadfaada36f385ed9fee87ebbc7b29
oizy404 <71779024+oizy404@users.noreply.github.com>
2020-12-28 19:27:47 +0800

    Create groupby_having.sql

---
+++ groupby_having.sql
@@ -0,0 +1,11 @@
+SELECT 
+    e.department_id,
+    department_name,
+    COUNT(employee_id) headcount
+FROM
+    employees e
+        INNER JOIN
+    departments d ON d.department_id = e.department_id
+GROUP BY e.department_id
+HAVING headcount > 5
+ORDER BY headcount DESC;
